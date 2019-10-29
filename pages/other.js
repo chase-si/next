714c40fa1b9d49f24e528075a27a5f76f60c32fd
chase <chase_si@163.com>
2019-10-29 14:12:15 +0800

    add the mobx

--- pages/other.js
+++ pages/other.js
@@ -4,7 +4,7 @@ import Page from '../components/Page'
 export default class Counter extends React.Component {
   render () {
     return (
-      <Page title='Index Page' linkTo='/other' />
+      <Page title='Other Page' linkTo='/' />
     )
   }
 }

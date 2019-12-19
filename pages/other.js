f6c385bd3a98d7b4bd06a3f87977541d8ea7d616
chase <chase_si@163.com>
2019-12-19 15:38:23 +0800

    save the request

--- pages/other.js
+++ pages/other.js
@@ -1,10 +1,29 @@
 import React from 'react'
-import Page from '../components/Page'
+import axios from 'axios'
+
 
 export default class Counter extends React.Component {
+  state = { data: 123 }
+
+  componentDidMount() {
+    const client = axios.create({ baseURL: 'http://47.100.175.176:3000' })
+    // 自定义 axios 实例
+
+
+    client.get('/login') // 第一次请求失败，第二次成功
+      .then(result => {
+        this.setState({data: result.data}) // 'ok'
+      })
+      .catch(err => {
+        console.dir(err)
+      })
+  }
+
   render () {
     return (
-      <Page title='Other Page' linkTo='/' />
+      <div>
+        {this.state.data}
+      </div>
     )
   }
 }

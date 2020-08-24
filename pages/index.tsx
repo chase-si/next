3f79bbb20a2f29b27981ed35215b370226fc23d2
Chase Wu <chase.wu@192.168.2.106>
2020-08-24 21:00:11 +0800

    add reducer

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,54 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 
 import { HomePage } from './containers/homePage'
 import { Select } from '../components/select'
 
-class App extends React.Component {
-  state = {
-    showHomePage: false
-  }
-
-  render() {
-    return (
-      <div >
-        {this.state.showHomePage && <HomePage /> }
-        <Select />
-        <button onClick={() => {this.setState({showHomePage: true})}}>
-          show  
-        </button>
-
-        <button onClick={() => {this.setState({showHomePage: false})}}>
-          hide  
-        </button>
-      </div>
-    )
+const reducer = (state, action) => {
+  console.log(state)
+  console.log(action)
+  switch (action.type) {
+    case '+':
+      return {
+        ...state,
+        count: state.count + 1
+      };
+    case '-':
+      return {count: state.count - 1};
+    default:
+      throw new Error();
   }
 }
+
+const App = () => {
+  const [states, dispatch] = useReducer(
+    reducer, {
+      count: 1,
+      test: 2
+    }
+  )
+  return (
+    <div >
+      <div>{states.count}</div>
+      <div onClick={() => dispatch({type: '+'})}>-</div>
+      <div>+</div>
+      <ChildCount count={states.count} />
+      <ChildTest test={states.test} />
+    </div>
+  )
+}
+
+const ChildCount = ({ count }) => {
+  console.log('rending count children')
+  return (
+    <div>count: {count}</div>
+  ) 
+}
+
+const ChildTest = ({ test }) => {
+  console.log('rending test children')
+  return (
+    <div>test: {test}</div>
+  ) 
+} 
 
 export default App
